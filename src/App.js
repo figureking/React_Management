3cb7794d0649756ec32619e21e0afe912da94f43
figureking <figureking2@gmail.com>
2020-06-22 19:30:09 +0900

    Create Customer.js change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+/*import logo from './logo.svg';*/
 import './App.css';
+import Customer from './Components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src ={logo} lat="logo" />
-      <h2> Let`s Develop MES System!!!!! </h2>
+const customers =[
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '홍길동',
+    'birthday' : ' 20200622',
+    'gender' : '남',
+    'job' : '영업부'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : ' 20200622',
+    'gender' : '남',
+    'job' : '영업부'
+    },
+    {
+      'id' : 3,
+      'image' : 'https://placeimg.com/64/64/3',
+      'name' : '홍길동',
+      'birthday' : ' 20200622',
+      'gender' : '남',
+      'job' : '영업부'
+      }
+]
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { 
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id} /* map key값 에러*/
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+
+            )
+          })
+        }
+      </div>
+      
+    );
+  }
 }
 
 export default App;
